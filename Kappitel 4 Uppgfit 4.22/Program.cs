2e093905e6cac7a1b882ef99c1b98694c837b7ab
MelkerBernhardsson <mebe05001@utb.vaxjo.se>
2022-10-19 11:23:47 +0200

    Add project files.

---
+++ Kappitel 4 Uppgfit 4.22/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace ____
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 10; i <= 30; i++)
+            {
+                Console.WriteLine(i);   
+            }
+            for (int g =200; g >= 180; g--)
+            {
+                Console.WriteLine(g);   
+            }
+            for (int j =1000; j <= 1400; j++)
+            {
+                Console.WriteLine(j);
+            }
+        }
+    }
+}
+
